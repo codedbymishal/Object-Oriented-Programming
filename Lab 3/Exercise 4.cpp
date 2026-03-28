7e830eeba8fbbba20ca1b13c87826fe8ae359ddb
Mishal Khalid <itsmishalk@gmail.com>
2026-03-28 11:26:30 +0500

    Exercise 4.cpp

--- Lab 3/Exercise 4.cpp
+++ Lab 3/Exercise 4.cpp
@@ -1,162 +1,215 @@
-
-#include<iostream>
+#include <iostream>
 #include <string>
 using namespace std;
-class bookType{
-	private:
-		string title;
-	    string authors[4];
-     	string publisher;
-     	string isbn;
-     	float price;
-     	int numofcopies;
-     	int numofauthors;
-    
-    public:
-    	
-    	bookType() // default constructor
-		{  
-    		title="";
-    		publisher="";
-    		isbn="";
-    		price=0.0;
-    		numofcopies=0;
-    		numofauthors=0;
-		}
-		~bookType() {} // destructor
-		
-		void setTitle(string t){
-			title=t;
-		}
-		
-     	string getTitle() const {
-     		return title;	
-		}
-		 
-		bool checkTitle(string t) const {
-			if(t==title) return true;
-			else return false;	
-		}
-		 
-		void setCopies(int noc){
-			numofcopies=noc;		
-		}
-		 	
-		int getCopies() const {
-			return numofcopies; 	
-		}
-		 
-		void updateNumOfCopies(int c){
-			numofcopies+=c;
-		}
-		
-		void setISBN(string i){
-			isbn=i;
-		}
-		
-		string getISBN() const{
-			return isbn;
-		}
-		
-		bool checkISBN(string i){
-			if (isbn==i) return true;
-			else return false;
-		}
-		
-		void setPrice(float p){
-			price=p;
-		}
-		
-		float getPrice() const{
-			return price;
-		}
-		
-		void setPublisher(string p){
-			publisher=p;
-		}
-		
-		string getPublisher() const{
-			return publisher;
-		}
-		
-		void setAuthors(string a[], int n){
-			if(n>4) n=4;
-			numofauthors=n;
-			for(int i=0;i<n;i++)
-			{
-				authors[i]=a[i];
-			}
-		}
-		void showAuthors() const {
-			for (int i = 0; i < numofauthors; i++) 
-			{
-				cout << authors[i];
-                if (i < numofauthors - 1)
-                cout << ", ";
-            }
-            cout << endl;
-    
-		}
-		void displayBook() const {
+
+class bookType
+{
+private:
+    string title;
+    string authors[4];
+    int numAuthors;
+    string publisher;
+    string ISBN;
+    double price;
+    int copiesInStock;
+
+public:
+    // Constructor
+    bookType()
+    {
+        title = "";
+        numAuthors = 0;
+        publisher = "";
+        ISBN = "";
+        price = 0.0;
+        copiesInStock = 0;
+    }
+
+    // --- Title ---
+    void setTitle(string t)
+    {
+        title = t;
+    }
+
+    string getTitle()
+    {
+        return title;
+    }
+
+    bool checkTitle(string t)
+    {
+        return (title == t);
+    }
+
+    // --- Authors ---
+    void setAuthors(string a[], int n)
+    {
+        numAuthors = n;
+        for (int i = 0; i < n; i++)
+            authors[i] = a[i];
+    }
+
+    void showAuthors()
+    {
+        for (int i = 0; i < numAuthors; i++)
+            cout << authors[i] << endl;
+    }
+
+    // --- Publisher ---
+    void setPublisher(string p)
+    {
+        publisher = p;
+    } // check??
+
+    string getPublisher()
+    {
+        return publisher;
+    }
+
+    // --- ISBN ---
+    void setISBN(string i)
+    {
+        ISBN = i;
+    }
+
+    string getISBN()
+    {
+        return ISBN;
+    }
+
+    bool checkISBN(string i)
+    {
+        return (ISBN == i);
+    }
+
+    // --- Price ---
+    void setPrice(double p)
+    {
+        price = p;
+    }
+
+    double getPrice()
+    {
+        return price;
+    }
+
+    // --- Copies ---
+    void setCopies(int c)
+    {
+        copiesInStock = c;
+    }
+
+    int getCopies()
+    {
+        return copiesInStock;
+    }
+
+    void updateCopies(int c)
+    {
+        copiesInStock += c;
+    }
+
+    // --- Display ---
+    void display()
+    {
         cout << "\nTitle: " << title << endl;
-        cout << "Authors: ";
-        showAuthors();
+        cout << "Authors: "<<endl;
+        showauthors();
         cout << "Publisher: " << publisher << endl;
-        cout << "ISBN: " << isbn << endl;
+        cout << "ISBN: " << ISBN << endl;
         cout << "Price: " << price << endl;
-        cout << "Copies in stock: " << numofcopies << endl;
+        cout << "Copies: " << copiesInStock << endl;
     }
 };
 int main()
 {
+    bookType books[100]; // lib has 100 books
 
-    bookType library[100];
-    int totalBooks = 2;
+    // Example: Enter data for 2 books
+    for (int i = 0; i < 2; i++)
+    {
+        string title, publisher, isbn;
+        double price;
+        int copies, n;
 
-   // Book 1
-    string authors1[2] = {"Ali Khan", "Sara Ahmed"};
-    library[0].setTitle("Object Oriented Programming");
-    library[0].setAuthors(authors1, 2);
-    library[0].setPublisher("Tech Press");
-    library[0].setISBN("111-222");
-    library[0].setPrice(550);
-    library[0].setCopies(10);
+        cout << "\nEnter title: ";
+        cin >> title;
+        books[i].setTitle(title);
 
-    // Book 2
-    string authors2[1] = {"John Smith"};
-    library[1].setTitle("Data Structures");
-    library[1].setAuthors(authors2, 1);
-    library[1].setPublisher("Education House");
-    library[1].setISBN("333-444");
-    library[1].setPrice(450);
-    library[1].setCopies(5);
+        cout << "Enter number of authors (max 4): ";
+        cin >> n;
 
-    // Search by title
-    string searchTitle = "Data Structures";
-    for (int i = 0; i < totalBooks; i++) 
-	{
-        if (library[i].checkTitle(searchTitle)) 
-		{
-            cout << "\nBook found by title:";
-            library[i].displayBook();
+        string a[4];
+        for (int j = 0; j < n; j++)
+        {
+            cout << "Enter author " << j + 1 << ": ";
+            cin >> a[j];
+        }
+        books[i].setAuthors(a, n);
+
+        cout << "Enter publisher: ";
+        cin >> publisher;
+        books[i].setPublisher(publisher);
+
+        cout << "Enter ISBN: ";
+        cin >> isbn;
+        books[i].setISBN(isbn);
+
+        cout << "Enter price: ";
+        cin >> price;
+        books[i].setPrice(price);
+
+        cout << "Enter copies: ";
+        cin >> copies;
+        books[i].setCopies(copies);
+    }
+
+    // Search by Title
+    string searchTitle;
+    cout << "\nEnter title to search: ";
+    cin >> searchTitle;
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (books[i].checkTitle(searchTitle))
+        {
+            cout << "\nBook Found:";
+            books[i].display();
         }
     }
 
-    // Search by ISBN
-    string searchISBN = "111-222";
-    for (int i = 0; i < totalBooks; i++) 
-	{
-        if (library[i].checkISBN(searchISBN)) 
-		{
-            cout << "\nBook found by ISBN:";
-            library[i].displayBook();
+    //Search by ISBN
+    string searchISBN;
+    cout << "\nEnter ISBN to search: ";
+    cin >> searchISBN;
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (books[i].checkISBN(searchISBN))
+        {
+            cout << "\nBook Found:";
+            books[i].display();
         }
     }
 
-    // Update copies
-    library[0].updateNumOfCopies(5);
-    cout << "\nAfter updating copies:";
-    library[0].displayBook();
+    //  Update Copies 
+    string updateISBN;
+    int newCopies;
+
+    cout << "\nEnter ISBN to update copies: ";
+    cin >> updateISBN;
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (books[i].checkISBN(updateISBN))
+        {
+            cout << "Enter number of copies to add: ";
+            cin >> newCopies;
+            books[i].updateCopies(newCopies);
+            cout << "Updated Book:";
+            books[i].display();
+        }
+    }
 
     return 0;
 }
